2aa3d8a958194f01952dbb96a418cee35a13561c
Tom Taylor <tom@tomtaylor.co.uk>
2020-07-25 20:33:07 +0100

    Add ExportChanged function
    The current implementation of Export always updates a feature, regardless of
    whether anything of meaning has changed, because it increments the lastmodified
    timestamp regardless.
    
    However, it's useful sometimes to only perform an export if there are changes
    that need writing out, such as when dealing with 2M postcodes, and trying to
    minimise the amount of noise git collects.
    
    ExportChanged makes this possible without needing to understand the ins and
    outs of preparing and formatting.

--- export.go
+++ export.go
@@ -19,17 +19,19 @@ type Feature struct {
 }
 
 func Export(feature []byte, opts options.Options, wr io.Writer) error {
-
 	var err error
 
-	feature, err = Prepare(feature, opts)
+	feature, err = prepare(feature, opts)
+	if err != nil {
+		return err
+	}
 
+	feature, err = updateLastModified(feature, opts)
 	if err != nil {
 		return err
 	}
 
 	feature, err = Format(feature, opts)
-
 	if err != nil {
 		return err
 	}
@@ -40,8 +42,62 @@ func Export(feature []byte, opts options.Options, wr io.Writer) error {
 	return err
 }
 
-func Prepare(feature []byte, opts options.Options) ([]byte, error) {
+// ExportChanged returns a boolean which indicates whether the file was changed
+// by comparing it to the `existingFeature` byte slice, before the lastmodified
+// timestamp is incremented. If the `feature` is identical to `existingFeature`
+// it doesn't write to the `io.Writer`.
+func ExportChanged(feature []byte, existingFeature []byte, opts options.Options, wr io.Writer) (changed bool, err error) {
+	changed = false
 
+	feature, err = prepare(feature, opts)
+	if err != nil {
+		return
+	}
+
+	feature, err = Format(feature, opts)
+	if err != nil {
+		return
+	}
+
+	changed = !bytes.Equal(feature, existingFeature)
+	if !changed {
+		return
+	}
+
+	feature, err = updateLastModified(feature, opts)
+	if err != nil {
+		return
+	}
+
+	r := bytes.NewReader(feature)
+	_, err = io.Copy(wr, r)
+
+	return
+}
+
+func Prepare(feature []byte, opts options.Options) ([]byte, error) {
+	var err error
+
+	feature, err = prepare(feature, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	feature, err = updateLastModified(feature, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return feature, nil
+}
+
+func Format(feature []byte, opts options.Options) ([]byte, error) {
+	var f format.Feature
+	json.Unmarshal(feature, &f)
+	return format.FormatFeature(&f)
+}
+
+func prepare(feature []byte, opts options.Options) ([]byte, error) {
 	var err error
 
 	feature, err = properties.EnsureWOFId(feature, opts.IDProvider())
@@ -86,17 +142,9 @@ func Prepare(feature []byte, opts options.Options) ([]byte, error) {
 		return nil, err
 	}
 
-	feature, err = properties.EnsureTimestamps(feature)
-
-	if err != nil {
-		return nil, err
-	}
-
 	return feature, nil
 }
 
-func Format(feature []byte, opts options.Options) ([]byte, error) {
-	var f format.Feature
-	json.Unmarshal(feature, &f)
-	return format.FormatFeature(&f)
+func updateLastModified(feature []byte, opts options.Options) ([]byte, error) {
+	return properties.EnsureTimestamps(feature)
 }
